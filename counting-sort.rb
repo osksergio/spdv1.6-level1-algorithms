c60869423310a40755becfbf2e19e295c8584937
Sergio Oseko <osk.sergio@gmail.com>
2021-10-26 10:02:23 -0300

    fix: totally wrong array syntax (I'm learning)

--- counting-sort.rb
+++ counting-sort.rb
@@ -1,22 +1,45 @@
+# Ruby program for counting sort
+ 
+# The main function that sort the given string arr[] in alphabetical order
 def countSort(arr)
-  output = [0 for i in (0...(arr.count - 1))]
 
-  count = [0 for i in (256)]
+  # The output character array that will have sorted arr  
+  output = Array.new(arr.length())
 
-  ans = ["" for _ in arr]
+  # Create a count array to store count of inidividual
+  # characters and initialize count array as 0
+  count = Array.new(256, 0)
 
-  for i in arr
-    count[ord(i)] += 1
+  # For storing the resulting answer since the string is immutable  
+  ans = Array.new()
+
+  # Store count of each character
+  for i in 0..(arr.length() - 1)
+    count[arr[i].ord] += 1
   end
 
-  for i in (0..(arr.count - 1))
-    output[count[ord(arr[i])] - 1] = arr[i]
-    count[ord(arr[i])] -= 1
+  # Change count[i] so that count[i] now contains actual
+  # position of this character in output array 
+  for i in 0..255
+    count[i] += count[i -1]
   end
 
-  for i in (0...(arr.count - 1))
+  # Build the output character array
+  for i in 0..arr.length() -1
+    output[count[arr[i].ord] - 1] = arr[i]
+    count[arr[i].ord] -= 1
+  end
+
+  # Copy the output array to arr, so tha arr now
+  # contais sorted characters
+  for i in 0..(arr.length() - 1)
     ans[i] = output[i]
   end
 
   return ans
 end
+
+# Driver program to test above function
+arr = "geeksforgeeks"
+ans = countSort(arr)
+puts "Sorted character array is " + ans.join("-")
