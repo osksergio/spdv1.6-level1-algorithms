e722326481e4d9a25b55d507b343663b3292cca4
Sergio Oseko <osk.sergio@gmail.com>
2021-10-27 07:12:55 -0300

    feat: minimum product (unfinished code)

---
+++ minimumProduct.rb
@@ -0,0 +1,15 @@
+# Ruby program to find maximum product of a subset.
+ 
+# def to find maximum product of a subset
+def minProductSubset(a, n)
+  if (n == 1) then
+    return a[0]
+
+  # Find count of negative numbers,
+  # count of zeros, maximum valued
+  # negative number, minimum valued
+  # positive number and product
+  # of non-zero numbers  
+
+  
+end
