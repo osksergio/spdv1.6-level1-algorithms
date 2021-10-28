e40c34d3ab0d83d543c6c91c5b157b1612bda822
Sergio Oseko <osk.sergio@gmail.com>
2021-10-28 07:05:13 -0300

    feat: algorithm missing characters

---
+++ missingCharacters.rb
@@ -0,0 +1,41 @@
+# Ruby program to find characters that needs to be added to make Pangram
+# Pangram is a sentence containing every letter in the English alphabet.
+MAX_CHAR = 26
+
+# Returns characters that needs to be added to make str
+def missingChars(str)
+
+    # A boolean array to store characters
+    # present in string.
+    #present = [false for i in 0..MAX_CHAR]
+    presentArr = [false];
+    
+    for i in 0..(MAX_CHAR - 2) do
+      #presentArr[i] = false
+      presentArr.push(false)
+    end
+
+    # Traverse string and mark characters present in string
+    for i in 0..(str.length() - 1) do
+      if (str[i] >= 'a' and str[i] <= 'z') then
+        presentArr[str[i].ord - 'a'.ord] = true
+      elsif (str[i] >= 'A' and str[i] <= 'Z') then
+        presentArr[str[i].ord - 'A'.ord] = true
+      end
+    end
+
+    # Store missing characters in alphabetic order.    
+    res = ""
+
+    for i in 0..(MAX_CHAR - 1) do
+      if (presentArr[i] == false) then
+        res += (i + 'a'.ord).to_s
+      end
+    end
+
+    return res
+end
+
+# Driver code
+str = "The quick brown fox jumps over the dog"
+puts (missingChars(str))
